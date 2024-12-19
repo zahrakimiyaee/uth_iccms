795b3563214ab033a61a3919d3826f806d046bf3
Zahra Kimiyaee <zahrakimiyaee2636@gmail.com>
2024-12-19 05:00:57 +0330

    صفحه درخواست دوره3

--- js/course.js
+++ js/course.js
@@ -40,7 +40,7 @@
 // }
 
 
-function master (click,click2) {
+function master (click) {
     let master_page = document.getElementById("master-page");
     let master_button = document.getElementById("master-button");
     let student_page = document.getElementById("student-page");
@@ -60,18 +60,30 @@ function master (click,click2) {
                     master_button.classList.add("master-select");
                     student_page.classList.add("d-none");
                     student_button.classList.add("student");
+                }else{
+                    if(student_page){
+                        if(student_page.classList.contains("d-none")){
+                            student_page.classList.remove("d-none");
+                            student_button.classList.remove("student");
+                            master_page.classList.add("master-class");
+                            master_button.classList.add("master-select");
+                        }else{
+                            if(!student_page.classList.contains("d-none")){
+                                student_page.classList.add("d-none");
+                                student_button.classList.add("student");
+                                master_page.classList.remove("master-class");
+                                master_button.classList.remove("master-select");
+                    console.log("im in");
+        
+                            }
+                        }
+                    }
                 }
-            }
-        }
-    }else{
-        if(click2){
-            if(student_page){
-                console.log("im in");
-                
             }
         }
     }
 }
+
 
 
 
