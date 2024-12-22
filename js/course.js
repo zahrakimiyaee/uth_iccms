14d780ec87ad55c8e77a6bd31befa6e8951dd106
Zahra Kimiyaee <zahrakimiyaee2636@gmail.com>
2024-12-22 18:43:49 +0330

    تکمیلی صفحه درخواست دوره 1

--- js/course.js
+++ js/course.js
@@ -3,85 +3,77 @@
 //     let master_button = document.getElementById("master-button");
 //     let student_page = document.getElementById("student-page");
 //     let student_button =  document.getElementById("student-button");
-    
-//     if(master_page){
-//         // student_page.classList.add("d-none");
-//         // student_button.classList.add("student");
-//         if(!master_page.classList.contains("master-class")){
-//             master_page.classList.add("master-class");
-//             master_button.classList.add("master-select");
-//         }
-//         else{
-//             console.log("im in");
-//             if(click){
-//                 master_page.classList.add("d-none");
+//     if(click){
+//         if(master_page){
+//             if(master_page.classList.contains("master-class")){
 //                 master_page.classList.remove("master-class");
 //                 master_button.classList.remove("master-select");
 //                 student_page.classList.remove("d-none");
-//                 student_button.classList.add("student-select");
+//                 student_button.classList.remove("student");
+//             }else{
+//                 if(!master_page.classList.contains("master-class")){
+//                     master_page.classList.add("master-class");
+//                     master_button.classList.add("master-select");
+//                     student_page.classList.add("d-none");
+//                     student_button.classList.add("student");
+//                 }else{
+//                     if(student_page){
+//                         if(student_page.classList.contains("d-none")){
+//                             student_page.classList.remove("d-none");
+//                             student_button.classList.remove("student");
+//                             master_page.classList.add("master-class");
+//                             master_button.classList.add("master-select");
+//                         }else{
+//                             if(!student_page.classList.contains("d-none")){
+//                                 student_page.classList.add("d-none");
+//                                 student_button.classList.add("student");
+//                                 master_page.classList.remove("master-class");
+//                                 master_button.classList.remove("master-select");        
+//                             }
+//                         }
+//                     }
+//                 }
 //             }
-//         }      
-//     }
-//     if(student_page){
-//         // master_page.classList.remove("master-class");
-//         // master_button.classList.add("master");
-//         if(!student_page.classList.contains("student-class")){
-//             master_page.classList.remove("master-class");
-//             student_page.classList.add("studnet-class");
-//             student_button.classList.add("student-select");
-//         }else
-//             if(click){
-//                 student_page.classList.remove("student-class");
-//                 student_button.classList.remove("student-select");
-//                 master_page.classList.add("master-class");
-//                 // master_page.classList.remove("d-none");
-//             }
+//         }
 //     }
 // }
-
-
 function master (click) {
     let master_page = document.getElementById("master-page");
     let master_button = document.getElementById("master-button");
-    let student_page = document.getElementById("student-page");
-    let student_button =  document.getElementById("student-button");
     if(click){
-        if(master_page){
-            if(master_page.classList.contains("master-class")){
+        if(!master_page.classList.contains("master-class")){
+            master_page.classList.add("master-class");
+            master_button.classList.add("master-select");
+            student(1);
+        }else{
+            if(click===1){
                 master_page.classList.remove("master-class");
                 master_button.classList.remove("master-select");
-                student_page.classList.remove("d-none");
-                student_button.classList.remove("student");
-                // student_button.classList.add("student-select");
-                // console.log("im in");
-            }else{
-                if(!master_page.classList.contains("master-class")){
-                    master_page.classList.add("master-class");
-                    master_button.classList.add("master-select");
-                    student_page.classList.add("d-none");
-                    student_button.classList.add("student");
-                }else{
-                    if(student_page){
-                        if(student_page.classList.contains("d-none")){
-                            student_page.classList.remove("d-none");
-                            student_button.classList.remove("student");
-                            master_page.classList.add("master-class");
-                            master_button.classList.add("master-select");
-                        }else{
-                            if(!student_page.classList.contains("d-none")){
-                                student_page.classList.add("d-none");
-                                student_button.classList.add("student");
-                                master_page.classList.remove("master-class");
-                                master_button.classList.remove("master-select");        
-                            }
-                        }
-                    }
-                }
+                click=0;
             }
         }
     }
 }
 
+function student (click) {
+    let student_page = document.getElementById("student-page");
+    let student_button = document.getElementById("student-button");
+    if(click){
+        if(student_page.classList.contains("student-class-d-none")){
+            student_page.classList.remove("student-class-d-none");
+            student_button.classList.remove("student")
+            master(1);
+        }else{
+            if(click===1){
+                student_page.classList.add("student-class-d-none");
+                student_button.classList.add("student");
+                click=0
+            }
+        }
+    }
+    
+}
+/**************************/
 function with_degree (select){
     let display_y = document.getElementById("display_y");
     if(select){
@@ -112,7 +104,7 @@ function without_degree (select){
         }
     }
 }
-
+/************************/
 function write_more (click){
     let textarea = document.getElementById("textarea");
     if(click){
@@ -125,5 +117,3 @@ function write_more (click){
         }
     }
 }
-
-
